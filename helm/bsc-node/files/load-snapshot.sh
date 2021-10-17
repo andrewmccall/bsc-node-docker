2b2fd5c184b93f92e5fc6fd6066a6b60dbfea926
Andrew McCall <andrew@andrewmccall.com>
2021-10-17 15:27:50 +0100

    Trying with the cloud SDK and a copy

--- helm/bsc-node/files/load-snapshot.sh
+++ helm/bsc-node/files/load-snapshot.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
+set -e
 
 # Loads the snapshot from a public GCS bucket and decompresses it as part of the
 # startup process. Assumes the tar is in the same format that BSC snapshots.
 
-echo "Mounting ${GCS_BUCKET}"
-mkdir -p ${SNAPSHOT_DIR}
-#gcsfuse $GCS_BUCKET ${SNAPSHOT_DIR}
+RUN /opt/geth/bin/geth init --datadir /opt/geth/node /opt/geth/config/genesis.json
 
+gsutil cp gs://crypto-bsc-snapshots/${SNAPSHOT_FILE} /opt/geth/node/
+ 
 echo "Extracting ${SNAPSHOT_FILE}"
-tar -zxvf ${SNAPSHOT_DIR}${SNAPSHOT_FILE} -C ${OUTPUT_LOCATION} --strip-components=2
+tar -zxvf /opt/geth/node/{SNAPSHOT_FILE} -C ${OUTPUT_LOCATION} --strip-components=2
