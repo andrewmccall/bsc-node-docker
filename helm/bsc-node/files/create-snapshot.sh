867ad7847698dea8aaf17b31e99f6540e93f2e5e
Andrew McCall <andrew@andrewmccall.com>
2021-10-17 13:31:14 +0100

    Mostly have the init stuff worked out.

--- helm/bsc-node/files/create-snapshot.sh
+++ helm/bsc-node/files/create-snapshot.sh
@@ -2,7 +2,7 @@
 
 # tars up a geth node to create a snapshot in the same way that the bsc snapshots are generates. 
 
-$BASE_DIR
-$OUTPUT_LOCATION
+echo "Creating snapshot of ${GETH_DIR}/geth in ${SNAPSHOT_DIR}"
 
-tar -zcvf snapshot.tar.gz geth/
+cd $GETH_DIR
+tar -zcvf ${SNAPSHOT_DIR}${SNAPSHOT_FILE} geth/chaindata/* --transform='s|^|server/data-seed/|'
--- helm/bsc-node/files/load-snapshot.sh
+++ helm/bsc-node/files/load-snapshot.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
+
 # Loads the snapshot from a public GCS bucket and decompresses it as part of the
-# startup process
+# startup process. Assumes the tar is in the same format that BSC snapshots.
 
 echo "Mounting ${GCS_BUCKET}"
-mkdir -p /mnt/gcs
-gcsfuse $GCS_BUCKET /mnt/gcs
+mkdir -p ${SNAPSHOT_DIR}
+#gcsfuse $GCS_BUCKET ${SNAPSHOT_DIR}
 
+echo "Extracting ${SNAPSHOT_FILE}"
+tar -zxvf ${SNAPSHOT_DIR}${SNAPSHOT_FILE} -C ${OUTPUT_LOCATION} --strip-components=2
