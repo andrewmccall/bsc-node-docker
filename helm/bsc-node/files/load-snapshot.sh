971056f2f6e99c53b4aeed0bc96a1c8831b9b3b4
Andrew McCall <andrew@andrewmccall.com>
2021-10-17 16:11:28 +0100

    Updated the dockerfile to init

--- helm/bsc-node/files/load-snapshot.sh
+++ helm/bsc-node/files/load-snapshot.sh
@@ -5,9 +5,9 @@ set -e
 # Loads the snapshot from a public GCS bucket and decompresses it as part of the
 # startup process. Assumes the tar is in the same format that BSC snapshots.
 
-RUN /opt/geth/bin/geth init --datadir /opt/geth/node /opt/geth/config/genesis.json
+/opt/geth/bin/geth init --datadir /opt/geth/node /opt/geth/config/genesis.json
 
-gsutil cp gs://crypto-bsc-snapshots/${SNAPSHOT_FILE} /opt/geth/node/
+#gsutil cp gs://crypto-bsc-snapshots/${SNAPSHOT_FILE} /opt/geth/node/
  
-echo "Extracting ${SNAPSHOT_FILE}"
-tar -zxvf /opt/geth/node/{SNAPSHOT_FILE} -C ${OUTPUT_LOCATION} --strip-components=2
+#echo "Extracting ${SNAPSHOT_FILE}"
+#tar -zxvf /opt/geth/node/{SNAPSHOT_FILE} -C ${OUTPUT_LOCATION} --strip-components=2
